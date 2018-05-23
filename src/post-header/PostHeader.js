3d00a4c3bf93112678a7077d6d23ec924931f5a1
Caio Brito <c.valtecio@gmail.com>
2018-05-23 16:14:25 -0300

    Componente de post

--- src/post-header/PostHeader.js
+++ src/post-header/PostHeader.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 
 import './PostHeader.scss';
+import photo from '../profile.png';
 
 export default class PostHeader extends Component {
   state = {};
@@ -8,7 +9,15 @@ export default class PostHeader extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Ola</h1>
+        <div className="container">
+          <div className="photoProfile">
+            <img className="thumb" src="../profile.png" />
+          </div>
+          <div className="user">
+            <div className="userName">Nome</div>
+            <small className="timePost">há 10 min</small>
+          </div>
+        </div>
       </Fragment>
     );
   }
